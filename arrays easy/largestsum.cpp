a7e67134a0591d2b62785071edacd81da571361d
Ritwija <ritwija05@gmail.com>
2023-07-06 20:09:17 +0530

    done

--- arrays easy/largestsum.cpp
+++ arrays easy/largestsum.cpp
@@ -1,2 +1,20 @@
-#include"bits/stdc++.h"
+#include<iostream>
+#include<vector>
 using namespace std;
+int largest(vector<int> &arr, int n)
+{
+    int max_no=arr[0];
+    for (int i = 1; i <n ; ++i)
+    {
+        if (max_no<arr[i])
+        {
+            max_no=arr[i];
+        }
+    }
+    return max_no;
+}
+int main()
+{
+    cout<<"Hello";
+    return 0;
+}
