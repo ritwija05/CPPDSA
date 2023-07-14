f0d007c0818db4e5bd073eda4817cc46e2f559f7
Ritwija <ritwija05@gmail.com>
2023-07-14 10:01:25 +0530

    done

---
+++ arrays easy/validparantheses.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+ bool isValid(string s)
+ {
+    stack<char>st;
+    for(auto c:s)
+    {
+        if(st.empty())
+        {
+            st.push(c);
+        }
+        else if(st.top()=='('&& c==')' || 
+        st.top()=='{' && c =='}'||
+        st.top()=='['&& c==']')
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(c);
+        }
+    }
+    if(st.empty())
+    return true ;
+    else
+    return false ;
+ }
