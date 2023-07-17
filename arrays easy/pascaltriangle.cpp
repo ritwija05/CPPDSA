baf3b0e2da4705b15fff0eaa028fbbbcfca6e569
Ritwija <ritwija05@gmail.com>
2023-07-17 22:40:09 +0530

    done

--- arrays easy/pascaltriangle.cpp
+++ arrays easy/pascaltriangle.cpp
@@ -1,2 +1,18 @@
 #include<iostream>
 #include<vector>
+using namespace std;
+vector<vector<int>> generate(int numRows)
+{
+    vector<vector<int>> ans;
+    for(int i=0;i<numRows;i++)
+    {
+        ans[i].resize(i + 1);
+        ans[i][0]=1;//nC0
+        ans[i][i]=1; //nCn
+        for(int j=1;j<i;j++)
+        {
+            ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+        }
+    }
+        return ans;
+}
